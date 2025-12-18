3e03b929234a1e9aa0a3916a9b42d3052553ea72
Jesus-Pons <jesponmor@alum.us.es>
2025-12-18 14:25:29 +0100

    fix: Corrección para eliminar Item

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,13 +1,11 @@
-from fastapi import APIRouter
-
-from app.api.routes import items, login, private, users, utils
+from app.api.routes import login, private, users, utils
 from app.core.config import settings
+from fastapi import APIRouter
 
 api_router = APIRouter()
 api_router.include_router(login.router)
 api_router.include_router(users.router)
 api_router.include_router(utils.router)
-api_router.include_router(items.router)
 
 
 if settings.ENVIRONMENT == "local":
